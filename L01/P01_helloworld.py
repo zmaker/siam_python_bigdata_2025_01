f5f7945795e3845c7d1398238dcbeac3c5be5020
Paolo Aliverti <paolo.aliverti@gmail.com>
2024-12-04 13:02:54 +0100

    L01

---
+++ L01/P01_helloworld.py
@@ -0,0 +1,30 @@
+# Primo programma
+# data: 4/12/2024
+
+'''
+Un commento molto lungo
+programma in python
+che stampa messaggi
+'''
+
+print("hello python") #stampo
+
+print('Hello Siam')
+
+print("Ciao,", "Mario", "come", "stai")
+print('Ciao,', "Mario", 'come', "stai")
+
+print("ciao 'Mario' come va?")
+print('ciao "Mario" come va?')
+print("temperatura di Milano:", 5)
+
+print("ciao", end="")
+print("ola")
+
+print("ciao", end="___")
+print("ola")
+
+print("aaa\nbbb\nccc")
+print(1,2,3,4,5,6)
+print(1,2,3,4,5,6, sep=";")
+
---
+++ L01/P02_variabili_e_operazioni.py
@@ -0,0 +1,37 @@
+temperatura = 5
+
+#temp di roma = 12 NO
+temp_di_roma = 12
+tempDiRoma = 14.5
+#12_stanza = 5
+
+nome = "Mario"
+
+print(nome)
+print("Roma=", tempDiRoma, "°C")
+
+print("T := ", temperatura)
+temperatura = 10.56
+print("T := ", temperatura)
+
+a = 10
+b = 20
+somma = a + b
+print("somma di a e b:", somma)
+
+prod = a * b
+print("prod di a e b:", prod)
+
+c = (a + b) * 5
+print(c)
+
+print("\ncount")
+count = 0
+print(count)
+count = count + 1
+print(count)
+count += 1
+print(count)
+count += 1
+print(count)
+

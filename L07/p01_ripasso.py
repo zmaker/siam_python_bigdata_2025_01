9bd794dbf772664ae55edb8b9c9e680d2a880e27
Paolo Aliverti <paolo.aliverti@gmail.com>
2025-01-09 11:51:53 +0100

    file csv

--- L07/p01_ripasso.py
+++ L07/p01_ripasso.py
@@ -15,5 +15,24 @@ a = 3
 b = 4
 z = somma(a, b)
 print(z)
-s = a + b
-print(s)
+
+#slicing
+testo = "quel ramo del lago di Como"
+print(testo)
+print( testo[2:6] ) #da 2 a 6
+print( testo[:6] )  #dall'inizio a 6
+print( testo[20:] )  #da 20 fino in fondo
+print( testo[20:-2] )
+print( testo[:-1] )
+
+numeri = [12,23,34,45,56,67,78]
+print(numeri)
+print(numeri[:5])
+print(numeri[5:])
+print(numeri[:-1])
+
+
+
+
+
+

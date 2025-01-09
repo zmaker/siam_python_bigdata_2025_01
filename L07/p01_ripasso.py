fb8d2a1c39da5ed55d0d17e74daf8659e9d35754
Paolo Aliverti <paolo.aliverti@gmail.com>
2025-01-09 11:28:01 +0100

    L07 - parte1

---
+++ L07/p01_ripasso.py
@@ -0,0 +1,19 @@
+def somma(a, b):
+    s = a + b
+    return s
+
+x = somma(10, 20)
+print(x)
+
+n = 1
+m = 2
+
+y = somma(n, m)
+print(y)
+
+a = 3
+b = 4
+z = somma(a, b)
+print(z)
+s = a + b
+print(s)
